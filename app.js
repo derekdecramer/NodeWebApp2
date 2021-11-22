1e94d8d8624efa0333c022b9919a563d8d9e146d
derekdecramer <derekdecramer@gmail.com>
2021-11-22 13:39:48 -0600

    gonna work this time

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
