0f41887c362552337f8ae047cc081bee53c955e9
derekdecramer <derekdecramer@gmail.com>
2021-11-22 00:41:31 -0600

    edited gitignore

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-/*var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');*/
-var greetingRouter = require('./routes/greeting');
-var buttonRouter = require('./routes/button');
 
 var app = express();
 
@@ -21,10 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/Greeting', greetingRouter);
-app.use('/Button', buttonRouter);*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
